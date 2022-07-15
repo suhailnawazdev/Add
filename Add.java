15029459f9fd3f022c965dc330f2e496c1ed78c0
suhailnawazdev <109364334+suhailnawazdev@users.noreply.github.com>
2022-07-15 20:57:39 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Add
+
+{
+
+public static void main (String args[])
+
+{
+
+int n1,n2,sum;
+
+System.out.println("Enter the value of 2 No");
+
+Scanner s = new Scanner (System.in);
+
+n1= s.nextInt();
+n2= s.nextInt();
+
+
+sum=n1 + n2;
+
+System.out.println("sum is" +sum);
+}
+}
